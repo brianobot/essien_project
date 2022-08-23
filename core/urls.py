2a4ad67341693f685f7570a4e75c66943c2b2de2
brian obot <brianobot9@gmail.com>
2022-08-23 13:21:25 +0100

    Changed the admin page title

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+# general settings for all admin pages registered in the project
+AdminSite.site_header = "MAadonna E-Library Admin"
+AdminSite.index_title = "Madonna E-Library Admin Homepage"
+AdminSite.site_title = "Madonna E-Library Admin"
+
 urlpatterns = [
     path('', include('library.urls')),
     path('admin/', admin.site.urls),
